09ce777a2f53e41c9191c37124d5071e39a8aedb
Philipp Balzarek <p.balzarek@googlemail.com>
2012-10-16 13:27:01 +0200

    complete header Serialization

--- source/Network/Stun.hs
+++ source/Network/Stun.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE PackageImports #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RecordWildCards #-}
 module Network.Stun where
 
 import Network
+import Control.Monad
 import Data.Word
 import Data.Serialize
 import Data.Bits
@@ -30,6 +31,7 @@ encodeMessageType method messageClass =
     .|. ((method .&. 0x70)  `shiftL` 1) -- next 3 bits are offset by 1
     .|. (c1 `shiftL` 8)                 -- bit 9 is class high bit
     .|. ((method .&. 0xf80) `shiftL` 2) -- highest 5 bits are offset by 2
+    -- most significant 2 bits reamin 0
   where
     (c1, c0) = case messageClass of
         Request    -> (0,0)
@@ -41,10 +43,10 @@ decodeMessageType :: Word16 -> (Method, MessageClass)
 decodeMessageType word = (method, mClass)
   where
     mClass = case (c1, c0) of
-        (False ,False) -> Request
-        (True  ,False) -> Success
-        (True  ,True) -> Failure
-        (False ,True) -> Indication
+        (False, False) -> Request
+        (True , False) -> Success
+        (True , True ) -> Failure
+        (False, True ) -> Indication
     c0 = testBit word 4
     c1 = testBit word 8
     method =
@@ -52,12 +54,37 @@ decodeMessageType word = (method, mClass)
         .|. ((word .&. 0xe0)  `shiftR` 1)  -- next 3 bits are offset by 1
         .|. ((word .&. 0x3e00) `shiftR` 2) -- highest 5 bits are offset by 2
 
+type TransactionID = (Word32, Word32, Word32)
 
+data MessageHeader = MessageHeader { messageMethod :: Method
+                                   , messageClass  :: MessageClass
+                                   , messageLength :: Word16
+                                   , transactionId     :: TransactionID
+                                   } deriving (Eq, Show)
 
-putMessage method messageClass messageLength = do
-    putWord16be (encodeMessageType method messageClass)
+putMessageHeader :: MessageHeader -> PutM ()
+putMessageHeader MessageHeader{..} = do
+    putWord16be (encodeMessageType messageMethod messageClass)
     putWord16be messageLength
     putWord32be cookie
+    let (tid1, tid2, tid3) = transactionId
+    putWord32be tid1
+    putWord32be tid2
+    putWord32be tid3
 
+getMessageHeader = do
+    (messageMethod, messageClass) <- decodeMessageType `liftM` getWord16be
+    messageLength <- getWord16be
+    cookie' <- getWord32be
+    guard $ cookie' == cookie
+    tid1 <- getWord32be
+    tid2 <- getWord32be
+    tid3 <- getWord32be
+    let transactionId = (tid1, tid2, tid3)
+    return MessageHeader{..}
+
+instance Serialize MessageHeader where
+    put = putMessageHeader
+    get = getMessageHeader
 
 showBits a = reverse [if testBit a i then '1' else '0' | i <- [0.. bitSize a - 1]]
--- source/Tests.hs
+++ source/Tests.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RecordWildCards #-}
 module Tests where
 
 import qualified Network.Stun as Stun
 import Test.QuickCheck
+import Data.Serialize
+
+import Control.Monad
 
 instance Arbitrary Stun.MessageClass where
     arbitrary = elements [ Stun.Request
@@ -19,5 +24,20 @@ checkEncDec method messageClass = let method' = method `mod` (2^12) in
 checkDecEnc word = let word' = word `mod` (2^14) in
         word' == (uncurry Stun.encodeMessageType $ Stun.decodeMessageType word')
 
+
 check1 = quickCheckWith (stdArgs{maxSuccess = 1000 }) checkEncDec
 check2 = quickCheckWith (stdArgs{maxSuccess = 1000 }) checkDecEnc
+
+instance Arbitrary Stun.MessageHeader where
+    arbitrary = do
+        messageMethod <- (`mod` (2^12)) `liftM` arbitrary
+        messageClass <- arbitrary
+        messageLength <- arbitrary
+        messageId <- arbitrary
+        return Stun.MessageHeader{..}
+
+
+checkSerializer header = decode (encode header) == Right header
+
+check3 = quickCheckWith (stdArgs{maxSuccess = 1000 })
+              (checkSerializer :: Stun.MessageHeader -> Bool)
